8b49d67b0072a5957f07cab0a7db1d676748d179
liu-zheng-yuan <lzhy1996@outlook.com>
2018-12-24 17:12:55 +0800

    添加debug

--- bztSpider/bztSpider/spiders/XcongFlashSpider.py
+++ bztSpider/bztSpider/spiders/XcongFlashSpider.py
@@ -3,20 +3,30 @@ import scrapy
 import datetime
 import json
 from bztSpider.items import NewsFlashItem
+from scrapy import Request,Spider
 
-class XcongFlashSpider(scrapy.Spider):
+class XcongFlashSpider(Spider):
     name = 'xcong'
     allowed_domains = ['wallstreetcn.com']
-    start_urls = ['https://api-prod.wallstreetcn.com/apiv1/content/lives']
+    api_url = "https://api-prod.wallstreetcn.com/apiv1/content/lives"
+    start_urls = ['https://api-prod.wallstreetcn.com/apiv1/content/lives?channel=xiaocong-channel&client=pc&cursor=&limit=50&first_page=false&accept_symbols=coin']
     # 建两个表快讯一个 新闻一个 多一个字段表示记录来自于哪个网站
+    # def start_requests(self):
+    #     channel = "xiaocong-channel"
+    #     client ="pc"
+    #     cursor=""
+    #     limit = "50"
+    #     first_page ="false"
+    #     accept_symbols = "coin"
+    #     yield Request(self.api_url.format(channel = channel,client=client,cursor=cursor,limit=limit,first_page=first_page,accept_symbols=accept_symbols),callback=self.parse)
+
     def parse(self, response:scrapy.http.response.Response):
         if response.status == 200:
-            rows = []
             patch_number = 50  # 一批插入的数量
-            data_json = json.load(response.text)
+            self.logger.debug(response.text)
+            data_json = json.loads(response.text)
             for i in range(patch_number):
                 data = data_json['data']['items'][i]
-                print(data)
                 item = NewsFlashItem()
                 item["news_flash_spider_source"] = "小葱"
                 item["news_flash_title"] = data["title"]
